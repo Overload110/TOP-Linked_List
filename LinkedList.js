a1f0390b6d176a7563e7b9cad9c53dcb1cc5ac3e
Overload110 <149197288+Overload110@users.noreply.github.com>
2024-01-20 12:56:03 -0600

    Add gets

--- LinkedList.js
+++ LinkedList.js
@@ -34,4 +34,38 @@ class LinkedList {
       this.head = node;
       this.size++;
     }
+
+    getSize() {
+      return this.size;
+    }
+  
+    getHead() {
+      return this.head;
+    }
+  
+    getTail() {
+      let current = this.head;
+  
+      while(current.next){
+        current = current.next;
+      }
+  
+      return current;
+    }
+  
+    getAt(index) {
+      let current = this.head;
+      let count = 0;
+  
+      while(current){
+        if(count === index) {
+          return current;
+        }
+  
+        count++;
+        current = current.next;
+      }
+  
+      return null;
+    }
 }
